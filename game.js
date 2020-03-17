9265b3a5667043ffdedb81d09accd9ae47463c1d
LucyIkonnikova-Skutsenia <jamie.ikonnikova@gmail.com>
2020-03-17 14:33:56 +0200

    Add heads up display to show progess and score

--- game.js
+++ game.js
@@ -1,5 +1,8 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
+
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -50,6 +53,9 @@ getNewQuestion = () => {
 	}
 	
 	questionCounter++;
+
+	questionCounterText.innerText = questionCounter + "/" + MAX_QUESTIONS;
+
 	const questionIndex = Math.floor(Math.random() * availableQuestions.length);
 	currentQuestion = availableQuestions[questionIndex];
 	question.innerText = currentQuestion.question;
@@ -72,6 +78,11 @@ choices.forEach(choice => {
 		const selectedAnswer = selectedChoice.dataset["number"];
 		
 		const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+		
+		if (classToApply === "correct") {
+			incrementScore(CORRECT_BONUS);
+		};
+
 		selectedChoice.parentElement.classList.add(classToApply);
 		setTimeout(() => {
 			selectedChoice.parentElement.classList.remove(classToApply);
@@ -79,5 +90,10 @@ choices.forEach(choice => {
 		}, 1000);
 	});
 });
+
+incrementScore = num => {
+	score += num;
+	scoreText.innerText = score;
+};
 
 startGame();
